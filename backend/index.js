c76c4c917e3a086c7fe7f64168c7b88f266e7a69
Khushi <gargkhushi70#gmail.com>
2025-05-15 22:23:57 +0530

    EMS

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const eventRoute = require("./controller/eventRoute");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb+srv://gargkhushi70:Atlas1234@cluster0.ju1tx.mongodb.net/emsdb");
+var db = mongoose.connection;
+db.on("open", () => console.log("Connected to DB"));
+db.on("error", () => console.log("Error occurred"));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.use('/eventRoute', eventRoute);
+app.listen(4000, () => {
+    console.log("Server started at 4000");
+})
